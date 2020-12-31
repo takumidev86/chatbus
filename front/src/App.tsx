b00c03379755d394c7c2f472cea23dc9a9a8c13f
takumidev86 <takumidevelopment86@gmail.com>
2020-12-31 15:32:24 +0900

    [Conbtainer]

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, CssBaseline } from "@material-ui/core";
+import { Container, CssBaseline, Grid } from "@material-ui/core";
 
 interface Props {}
 
@@ -7,8 +7,19 @@ const App: React.FC<Props> = (props) => {
   // const [count, setCount] = useState<number>(0);
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xl">
       <CssBaseline />
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          bus
+        </Grid>
+        <Grid item xs={6}>
+          bus
+        </Grid>
+        <Grid item xs={3}>
+          bus
+        </Grid>
+      </Grid>
     </Container>
   );
 };
